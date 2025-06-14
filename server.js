5f06c307276c874f0b3bd5353520cbfda0ccf3c9
sandesh <sandeshbhatta218@gmail.com>
2025-06-14 12:25:16 +0545

    exporting it to use it as a serverless function.

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const cors = require('cors');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Read data from JSON file
 const iceCreams = JSON.parse(fs.readFileSync('./icecreams.json', 'utf-8'));
 
+// Route for the API
 app.get('/api/icecreams', (req, res) => {
     res.json(iceCreams);
 });
 
-app.listen(PORT, () => {
-    console.log(`🍦 Ice cream API running at http://localhost:${PORT}/api/icecreams`);
-});
+//  listen on port
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`🍦 Ice cream API running at http://localhost:${PORT}/api/icecreams`);
+    });
+}
+
+//Export app for Vercel (this is required)
+module.exports = app;
